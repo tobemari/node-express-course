392e088283d31ca88a485d23b7a9f3818f4434bb
Maria Bogomolova <domarkasmv@gmail.com>
2025-04-11 14:30:42 -0400

    Optional additional assignment. Added auth middleware, logon, logoff, test routes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,16 +1,45 @@
 const express = require('express')
 const app = express()
-const { products, people } = require('./data');
+const { products, people } = require('./data')
 const peopleRouter = require('./routes/people')
 const logger = require('./logger')
+const cookieParser = require('cookie-parser')
 
 app.use(express.static('./methods-public'))
 app.use(logger)
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cookieParser())
 app.use('/api/v1/people', peopleRouter)
 
-app.get('/about', (req,res)=>{
+const auth = ((req, res, next) => {
+    const name = req.cookies.name
+    if(!name){
+        return res.status(401).json({message: "unauthorized"})
+    }
+    req.user = name
+    next()
+})
+
+app.post('/logon', (req, res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({message: "Please provide a name"})
+    }
+    res.cookie('name', req.body.name)
+    res.status(201).json({message:`Hello ${name}`})
+})
+
+app.delete('/logoff', (req, res)=>{
+    res.clearCookie('name')
+    res.status(200).json({message: "You are logged off"})
+})
+
+app.get('/test', auth, (req, res)=>{
+    res.status(200).json({message: `Welcome ${req.user}`})
+})
+
+app.get('/about', (req,res,)=>{
     res.status(200).send("<h1>This is About Page</h1>")
 })
 
